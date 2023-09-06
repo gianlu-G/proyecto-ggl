4b54ebe42a692d32c55df11a52cd58e96163df4e
Alvaro-67 <giustialvin123@gmail.com>
2023-09-06 14:23:59 -0300

    Read more function
    Cambie el codigo con una funcion de leer mas que funciona

--- assets/Noti.js
+++ assets/Noti.js
@@ -1,72 +1,92 @@
-let areElementsVisible = false;
+/*let areElementsVisible = false;
 let areElementsVisible2 = false;
-    let shownElements = []; // Arreglo para guardar los elementos adicionales mostrados
-    let shownElements2 = [];
+let shownElements = []; // Arreglo para guardar los elementos adicionales mostrados
+let shownElements2 = [];
 
-    function toggleElements() {
-      const mainContainer = document.getElementById('mainContainer');
-      const toggleButton = document.getElementById('toggleButton');
+function toggleElements() {
+  const mainContainer = document.getElementById('mainContainer');
+  const toggleButton = document.getElementById('toggleButton');
 
-      // Mostrar u ocultar los elementos adicionales
-      const hiddenElements = mainContainer.querySelectorAll('.hidden');
-      if (!areElementsVisible) {
-        // Si los elementos están ocultos, los mostramos y los agregamos al arreglo
-        hiddenElements.forEach((element) => {
-          element.classList.remove('hidden');
-          shownElements.push(element);
-        });
-      } else {
-        // Si los elementos están visibles, los ocultamos y los eliminamos del arreglo
-        shownElements.forEach((element) => {
-          element.classList.add('hidden');
-        });
-        shownElements = [];
-      }
-
-      // Cambiar el estado de los elementos visibles
-      areElementsVisible = !areElementsVisible;
-
-      // Mover el botón al final del contenedor
-      mainContainer.appendChild(toggleButton);
-    }
-    function toggleElements2() {
-    const mainContainer2 = document.getElementById('secondContainer');
-    const toggleButton2 = document.getElementById('toggleButton2');
-
-      // Mostrar u ocultar los elementos adicionales
-      const hiddenElements2 = mainContainer2.querySelectorAll('.oculto');
-      if (!areElementsVisible2) {
-        // Si los elementos están ocultos, los mostramos y los agregamos al arreglo
-        hiddenElements2.forEach((element) => {
-          element.classList.remove('oculto');
-          shownElements2.push(element);
-        });
-      } else {
-        // Si los elementos están visibles, los ocultamos y los eliminamos del arreglo
-        shownElements2.forEach((element) => {
-          element.classList.add('oculto');
-        });
-        shownElements2 = [];
-      }
-
-      // Cambiar el estado de los elementos visibles
-      areElementsVisible2 = !areElementsVisible2;
-
-      // Mover el botón al final del contenedor
-      mainContainer2.appendChild(toggleButton2);
-    }
-    document.addEventListener("DOMContentLoaded", function() {
-      function toggleElements(containerId) {
-        const container = document.getElementById(containerId);
-        const visibleElements = container.querySelectorAll('.element:not(.hidden)');
-
-        visibleElements.forEach((element) => {
-          element.classList.toggle('hidden');
-        });
-
-        const elementHeight = visibleElements.length > 0 ? visibleElements[0].offsetHeight : 0;
-        const newContainerHeight = visibleElements.length * elementHeight;
-
-        container.style.maxHeight = newContainerHeight + 'px';
-      }
+  // Mostrar u ocultar los elementos adicionales
+  const hiddenElements = mainContainer.querySelectorAll('.hidden');
+  if (!areElementsVisible) {
+    // Si los elementos están ocultos, los mostramos y los agregamos al arreglo
+    hiddenElements.forEach((element) => {
+      element.classList.remove('hidden');
+      shownElements.push(element);
     });
+  } else {
+    // Si los elementos están visibles, los ocultamos y los eliminamos del arreglo
+    shownElements.forEach((element) => {
+      element.classList.add('hidden');
+    });
+    shownElements = [];
+  }
+
+  // Cambiar el estado de los elementos visibles
+  areElementsVisible = !areElementsVisible;
+
+  // Mover el botón al final del contenedor
+  mainContainer.appendChild(toggleButton);
+}
+function toggleElements2() {
+  const mainContainer2 = document.getElementById('secondContainer');
+  const toggleButton2 = document.getElementById('toggleButton2');
+
+  // Mostrar u ocultar los elementos adicionales
+  const hiddenElements2 = mainContainer2.querySelectorAll('.oculto');
+  if (!areElementsVisible2) {
+    // Si los elementos están ocultos, los mostramos y los agregamos al arreglo
+    hiddenElements2.forEach((element) => {
+      element.classList.remove('oculto');
+      shownElements2.push(element);
+    });
+  } else {
+    // Si los elementos están visibles, los ocultamos y los eliminamos del arreglo
+    shownElements2.forEach((element) => {
+      element.classList.add('oculto');
+    });
+    shownElements2 = [];
+  }
+
+  // Cambiar el estado de los elementos visibles
+  areElementsVisible2 = !areElementsVisible2;
+
+  // Mover el botón al final del contenedor
+  mainContainer2.appendChild(toggleButton2);
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  function toggleElements(containerId) {
+    const container = document.getElementById(containerId);
+    const visibleElements = container.querySelectorAll('.element:not(.hidden)');
+
+    visibleElements.forEach((element) => {
+      element.classList.toggle('hidden');
+    });
+
+    const elementHeight = visibleElements.length > 0 ? visibleElements[0].offsetHeight : 0;
+    const newContainerHeight = visibleElements.length * elementHeight;
+
+    container.style.maxHeight = newContainerHeight + 'px';
+  }
+});*/
+
+const parentContainer = document.querySelector('.read-more-container');
+
+parentContainer.addEventListener('click', event => {
+
+  const current = event.target;
+
+  const isReadMoreBtn = current.className.includes('read-more-btn');
+
+  if (!isReadMoreBtn) return;
+
+  const currentText = event.target.parentNode.querySelector('.read-more-text');
+
+  currentText.classList.toggle('read-more-text--show');
+
+  current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+
+})
